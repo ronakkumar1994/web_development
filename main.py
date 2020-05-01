8ca1b6d7231d1d615171081546930cabee0f4c81
Ronak <ronakkumar1984@gmail.com>
2020-05-02 01:31:32 +0530

    temprory

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy,request
 from datetime import datetime
+from flask_mail import Mail
+import json
+
+with open('config.json','r') as c:
+    params=json.load(c)["params"]
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/test_web'
+app.config.update(
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT='465',
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME=params['gmail-user'],
+    MAIL_PASSWORD=params['gmail-password']
+)
+mail = Mail(app)
+
+if (params['local_server']):
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+
 db = SQLAlchemy(app)
 
 
 class Contact(db.Model):
-    '''sr.no, name, email, phone, message, date'''
     srno = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(25), unique=True, nullable=False)
-    phone = db.Column(db.String(10),  nullable=False)
+    phone = db.Column(db.String(11),  nullable=False)
     message = db.Column(db.String(80), nullable=False)
     date = db.Column(db.String(12), nullable=False)
 
@@ -40,6 +57,10 @@ def contact():
         entry = Contact(name=name, email=email, phone=phone,message=message,date=datetime.now())
         db.session.add(entry)
         db.session.commit()
+        mail.send_message('new enquiry from '+name,
+                          sender=email,recipients=[params['gmail-user']],
+                          body=message+"\n"+phone+"\n"+email
+                          )
     return render_template('contact.html')
 
 
